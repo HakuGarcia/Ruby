48b677efeb19eda6a653da0ea32b30d1b2bf426f
HakuAkai <102476175+HakuAkai@users.noreply.github.com>
2023-08-17 23:15:02 -0300

    Add files via upload

---
+++ biblioteca.rb
@@ -0,0 +1,10 @@
+print "Escreva seu nome "
+    nome = gets.chomp
+
+print "Escreva seu sobrenome "
+    sobrenome = gets.chomp
+
+print "Por favor, escreva sua idade "
+    idade = gets.chomp
+
+puts "Seu nome é #{nome} #{sobrenome} e você tem #{idade} anos"
